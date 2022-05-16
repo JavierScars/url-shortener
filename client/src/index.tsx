f9d4322078f541a2ad6eae3106f49b36ea3bfcd7
Fernando G <ferjgmorales25@gmail.com>
2022-05-16 13:12:38 -0500

    client - router refactor

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,7 @@
 import ReactDOM from 'react-dom/client';
-import { Route, Navigate } from 'react-router-dom';
-import { Home } from './pages/home/Home';
 import reportWebVitals from './reportWebVitals';
 import { Router } from './router/Router';
 import { Box, ChakraProvider } from '@chakra-ui/react'
-import { Redirect } from './pages/redirect/Redirect';
-import { Login } from './pages/login/Login';
 import { UserContextProvider } from './context/userContext';
 
 const root = ReactDOM.createRoot(
@@ -15,12 +11,7 @@ root.render(
   <ChakraProvider>
     <UserContextProvider>
       <Box minH="100vh" minW="100vw">
-        <Router>
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/:hash' element={<Redirect />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Router>
+        <Router />
       </Box>
     </UserContextProvider>
   </ChakraProvider>
--- client/src/router/Router.tsx
+++ client/src/router/Router.tsx
@@ -1,14 +1,29 @@
-import * as React from 'react';
 import { BrowserRouter, Routes } from "react-router-dom";
+import { Route, Navigate } from 'react-router-dom';
+import { Home } from '../pages/home/Home';
+import { Login } from '../pages/login/Login';
+import React, { FC, useContext } from 'react';
+import { UserContext } from '../context/userContext';
+import { Redirect } from '../pages/redirect/Redirect';
 
-type Props = {
-    children?: React.ReactNode
-};
+interface ProtectedRouteProps {
+    Component: FC,
+    condition: Boolean,
+    redirectPath: string
+}
+const ProtectedRoute: FC<ProtectedRouteProps> = ({ Component, condition, redirectPath, ...rest }) => {
+    return condition ? <Component {...rest} /> : <Navigate to={redirectPath} />
+}
 
-export const Router: React.FC<Props> = ({ children }) => {
+export const Router: React.FC = () => {
+    const { user } = useContext(UserContext);
     return <BrowserRouter>
         <Routes>
-            {children}
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<ProtectedRoute Component={Login} condition={!user} redirectPath='/' />} />
+            <Route path='/profile' element={<ProtectedRoute Component={Login} condition={!!user} redirectPath='/login' />} />
+            <Route path='/:hash' element={<Redirect />} />
+            <Route path="*" element={<Navigate to="/" />} />
         </Routes>
     </BrowserRouter>
 }
