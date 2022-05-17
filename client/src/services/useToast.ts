5b1a8c005c7587a351c3ee374029738f21198a91
Fernando G <ferjgmorales25@gmail.com>
2022-05-17 11:30:55 -0500

    mend

--- client/src/services/useToast.ts
+++ client/src/services/useToast.ts
@@ -1,5 +1,6 @@
 import { useToast as chakraUseToast } from "@chakra-ui/react";
 
+// use custom hook so it wont crash on test enviroment
 export const useToast = () => {
     return process.env.NODE_ENV === 'test' ? () => { } : chakraUseToast();
 }
